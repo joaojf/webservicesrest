c1c6b10094d32b54f9e0f98edbbd4db9c8602e2a
João Ferreira <joaovitor13cv@hotmail.com>
2021-06-07 21:04:39 -0300

    Backend project created

---
+++ backend/src/main/java/com/crud/webservicesrest/WebservicesrestApplication.java
@@ -0,0 +1,13 @@
+package com.crud.webservicesrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesrestApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/crud/webservicesrest/WebservicesrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.webservicesrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservicesrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
